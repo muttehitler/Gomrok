78f68150da637b655b43ef96751f10fe05aa3b38
ben-aslan <kiziltahaaslan@gmail.com>
2025-05-05 10:58:16 +0330

    feat: add panel update on ui

--- ui/src/components/editPanel/editPanel.tsx
+++ ui/src/components/editPanel/editPanel.tsx
@@ -3,7 +3,7 @@
 import { useTranslations } from "next-intl";
 import { Dispatch, FC, SetStateAction, useEffect, useRef, useState } from "react";
 import './style.css'
-import { addPanel, getPanel, testConnection } from "@/actions/panel.action";
+import { getPanel, testConnection, updatePanel } from "@/actions/panel.action";
 import { generateCsrfToken } from "@/lib/utils/csrf.helper";
 import { getCookie } from "@/lib/utils/cookie.helper";
 
@@ -26,6 +26,8 @@ export const EditPanel: FC<EditPanelProp> = ({ id, visableState }: EditPanelProp
     const t = useTranslations('i18n');
 
     const formRef = useRef<HTMLFormElement>(null)
+
+    const [testConnectionText, setTestConnectionText] = useState(t('test'))
 
     const [csrfToken, setCsrfToken] = useState('')
     const [panelName, setPanelName] = useState('')
@@ -54,12 +56,33 @@ export const EditPanel: FC<EditPanelProp> = ({ id, visableState }: EditPanelProp
         })()
     }, [])
 
+    const testConnectionHandler = async () => {
+        if (!formRef.current) return
+
+        const dataAsJson = Object.fromEntries(new FormData(formRef.current).entries())
+
+        setTestConnectionText(t("testing"))
+
+        const result = await testConnection(dataAsJson)
+
+        if (result == 200) {
+            setTestConnectionText(t("successed"))
+            return;
+        }
+
+        setTestConnectionText(t("fail"))
+    }
+
     const addPanelHandler = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
 
-        const dataAsJson = Object.fromEntries(new FormData(e.currentTarget).entries())
+        const formData = new FormData(e.currentTarget)
 
-        const result = await addPanel(dataAsJson)
+        formData.append('id', id)
+
+        const dataAsJson = Object.fromEntries(formData.entries())
+
+        const result = await updatePanel(dataAsJson)
 
         alert(result)
     }
@@ -72,36 +95,39 @@ export const EditPanel: FC<EditPanelProp> = ({ id, visableState }: EditPanelProp
                         <div className='add-panel-field-div'>
                             <div className='panel-field'>
                                 <label htmlFor="panel_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{t('panel-name')}</label>
-                                <input value={panelName} name='name' type="text" id="panel_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={t('panel-name')} required />
+                                <input defaultValue={panelName} name='name' type="text" id="panel_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={t('panel-name')} required />
                             </div>
                             <div className='panel-field'>
                                 <label htmlFor="panel_types" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{t('panel-type')}</label>
-                                <select value={panelType} name='type' id="panel_types" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <select defaultValue={panelType} name='type' id="panel_types" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                     <option>{t('choose-a-panel-type')}</option>
                                     <option value="marzneshin">{t('marzneshin')}</option>
                                 </select>
                             </div>
                             <div className='panel-field'>
                                 <label htmlFor="panel_url" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{t('panel-url')}</label>
-                                <input value={panelUrl} name="url" type="text" id="panel_url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={t('panel-url')} required />
+                                <input defaultValue={panelUrl} name="url" type="text" id="panel_url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={t('panel-url')} required />
                             </div>
                             <div className='panel-field'>
                                 <label htmlFor="panel_username" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{t('panel-username')}</label>
-                                <input value={panelUsername} name="username" type="text" id="panel_username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={t('panel-username')} required />
+                                <input defaultValue={panelUsername} name="username" type="text" id="panel_username" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={t('panel-username')} required />
                             </div>
                             <div className='panel-field'>
                                 <label htmlFor="panel_password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{t('panel-password')}</label>
-                                <input value={panelPassword} name="password" type="password" id="panel_password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={t('panel-password')} required />
+                                <input defaultValue={panelPassword} name="password" type="password" id="panel_password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={t('panel-password')} required />
                             </div>
                             <div className='panel-field'>
                                 <label htmlFor="panel_weight" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{t('panel-weight')}</label>
-                                <input value={panelWeight} name="weight" type="number" id="panel_weight" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={t('panel-weight')} required />
+                                <input defaultValue={panelWeight} name="weight" type="number" id="panel_weight" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={t('panel-weight')} required />
                             </div>
                             <input name="csrf" type="hidden" value={csrfToken} />
                         </div>
                         <div className='flex'>
                             <button onClick={() => { setVisablity(false) }} type="button" className='cancel-button bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full'>
                                 {t('cancel')}
+                            </button>
+                            <button onClick={testConnectionHandler} type="button" className='add-button ml-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'>
+                                {testConnectionText}
                             </button>
                             <button className='add-button ml-auto bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full'>
                                 {t('update')}
--- ui/src/patterns/panel.pattern.ts
+++ ui/src/patterns/panel.pattern.ts
@@ -2,5 +2,6 @@ export const PANEL_PATTERNS = {
     TEST_CONNECTION: '/panel/test_connection',
     ADD: '/panel/add',
     GET_LIST: '/panel',
-    GET:'/panel/'
+    GET: '/panel/',
+    UPDATE: '/panel/'
 }
