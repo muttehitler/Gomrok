1a3e6f0a913519bd8cfe2a8730cef0906a66d69e
ben-aslan <kiziltahaaslan@gmail.com>
2025-05-16 17:19:02 +0330

    feat: add order model

--- app/apps/order/src/order.module.ts
+++ app/apps/order/src/order.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
+import Order, { OrderSchema } from './models/concrete/order';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.AUTH_MONGO_STRING?.toString() ?? '', { dbName: 'orderdb' }),
+    MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }])
+  ],
   controllers: [OrderController],
   providers: [OrderService],
 })
-export class OrderModule {}
+export class OrderModule { }
