1cf4439a416a5fa745afce76ddae75facaf19fcf
ben-aslan <kiziltahaaslan@gmail.com>
2025-05-23 12:38:50 +0330

    fix: add order name in ui

--- ui/src/app/product/detail/page.tsx
+++ ui/src/app/product/detail/page.tsx
@@ -117,7 +117,7 @@ export default function Detail() {
                         <div className='add-panel-field-div'>
                             <div className='panel-field'>
                                 <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{t('product-name')}</label>
-                                <input {...register('name')} name='orderName' type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={t('product-name')} required />
+                                <input {...register('name')} name='name' type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={t('product-name')} required />
                                 {errors.name && <p style={{ color: 'red' }}>{errors.name.message}</p>}
                             </div>
                             <input {...register('csrf')} name="csrf" type="hidden" value={csrfToken} />
