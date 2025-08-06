9fe186e26380d4b7605cece6aa0d3012ab77ce1a
ben-aslan <kiziltahaaslan@gmail.com>
2025-08-06 14:18:42 +0330

    feat(panel): marzban panel option on ui

--- ui/src/components/addPanel/addPanel.tsx
+++ ui/src/components/addPanel/addPanel.tsx
@@ -102,6 +102,7 @@ export const AddPanel: FC = () => {
                                 <select {...register('type')} name='type' id="panel_types" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                     <option>{t('choose-a-panel-type')}</option>
                                     <option value="marzneshin">{t('marzneshin')}</option>
+                                    <option value="marzban">{t('marzban')}</option>
                                 </select>
                                 {errors.type && <p style={{ color: 'red' }}>{errors.type.message}</p>}
                             </div>
--- ui/src/components/editPanel/editPanel.tsx
+++ ui/src/components/editPanel/editPanel.tsx
@@ -133,6 +133,7 @@ export const EditPanel: FC<EditPanelProp> = ({ id, visableState }: EditPanelProp
                                 <select {...register('type')} defaultValue={panelType} name='type' id="panel_types" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                     <option>{t('choose-a-panel-type')}</option>
                                     <option value="marzneshin">{t('marzneshin')}</option>
+                                    <option value="marzban">{t('marzban')}</option>
                                 </select>
                                 {errors.type && <p style={{ color: 'red' }}>{errors.type.message}</p>}
                             </div>
