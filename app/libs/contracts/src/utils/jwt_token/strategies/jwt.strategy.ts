9deabff39447eb17389b2f8217870ff01d8919c3
ben-aslan <kiziltahaaslan@gmail.com>
2025-08-20 01:14:49 +0330

    feat(http-gateway): exception endpoint log for jwt

--- app/libs/contracts/src/utils/jwt_token/strategies/jwt.strategy.ts
+++ app/libs/contracts/src/utils/jwt_token/strategies/jwt.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { ForbiddenException, Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import PassportUser from "../passportUser";
@@ -16,7 +16,16 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
     async validate(req: any, payload: PassportUser) {
         if (!req.claims || req.claims?.filter(x => payload.claims.includes(x)).length <= 0)
-            throw UnauthorizedException
+            throw {
+                statusCode: 403,
+                message: "Forbidden",
+                data: {
+                    method: req.method,
+                    path: req.originalUrl,
+                    baseUrl: req.baseUrl,
+                    route: req.route?.path
+                }
+            }
         return payload;
     }
 }
