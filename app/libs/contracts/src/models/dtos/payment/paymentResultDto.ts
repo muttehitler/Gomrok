54c17bac82af278d526a192c6777b63ee0a52d6f
ben-aslan <kiziltahaaslan@gmail.com>
2025-08-20 14:50:58 +0330

    feat(payment): more payment detail

--- app/libs/contracts/src/models/dtos/payment/paymentResultDto.ts
+++ app/libs/contracts/src/models/dtos/payment/paymentResultDto.ts
@@ -1,5 +1,20 @@
+import UserDto from "../user/userDto"
+
 export default interface PaymentResultDto {
     id: string
+
+    status?: boolean
+    completed?: boolean
+    user?: UserDto
+    currency?: string
+    cardNumber?: string
+    hash?: string
+
+    paymentMethod?: string
+
+    createdAt?: Date;
+    updatedAt?: Date;
+
     walletAddress: string
     amount: number
 }
